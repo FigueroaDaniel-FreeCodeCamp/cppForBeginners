1b3b18399a7ce761d783d76d9e82788a597642b8
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-26 19:43:13 -0500

    Working with strings

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main() {
 
-	char grade = 'A';
-	string phrase = "Giraffe Academy";
-	int age = 50;
-	double gpa = 4.3;
-	bool isMale = true;
+	//===========================================
+	cout << "Girraf Academy \n"; // using \n
+	cout << "Hello" << endl;
+
+	//===========================================
+	string phrase = "Hello Daniel";
+	cout << phrase << endl; // Hello Daniel
+	cout << phrase.length() << endl; // 12
+	cout << phrase[6] << endl; // D
+	phrase[6] = 'C';
+	cout << phrase << endl; // Hello Caniel
+	cout << phrase.find("Caniel", 0) << endl; // 6 // find recibe 2 argumentos
+	string subString = phrase.substr(6, 11);
+	cout << subString; // Caniel
+
 
 	return 0;
 }
