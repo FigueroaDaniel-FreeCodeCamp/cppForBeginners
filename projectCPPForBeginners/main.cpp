a1ade8bdefc5555d077bf508fffb329b34c6c3c2
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-28 19:22:58 -0500

    Bulding a calculator

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -4,17 +4,13 @@
 using namespace std;
 
 int main() {
-	
-	/*int age;
-	cout << "Enter your age: ";
-	cin >> age;
-	cout << "You are " << age << " years old.";*/
 
-	// Working with strings
-	string name;
-	cout << "Enter your name: ";
-	getline(cin, name);
-	cout << "Hello " << name;
+	double num1, num2;
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	cout << num1 + num2;
 
 	return 0;
 }
