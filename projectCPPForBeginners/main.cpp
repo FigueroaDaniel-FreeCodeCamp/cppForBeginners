3feb24b2496f187ddb0a8e7b3498482ffd10ccb5
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-28 19:15:09 -0500

    Getting user input

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include <cmath> // To use manth functions
+#include <string> // To use getline
 
 using namespace std;
 
 int main() {
+	
+	/*int age;
+	cout << "Enter your age: ";
+	cin >> age;
+	cout << "You are " << age << " years old.";*/
 
-	cout << pow(2, 3) << endl; // 8
-	cout << sqrt(81) << endl; // 9
-	cout << round(4.3) << endl; // 4
-	cout << round(4.6) << endl; // 5
-	cout << ceil(4.6) << endl; // 5
-	cout << ceil(4.1) << endl; // 5
-	cout << floor(4.8) << endl; // 4
-	cout << fmax(3, 10); // 10
- 
+	// Working with strings
+	string name;
+	cout << "Enter your name: ";
+	getline(cin, name);
+	cout << "Hello " << name;
+
 	return 0;
 }
