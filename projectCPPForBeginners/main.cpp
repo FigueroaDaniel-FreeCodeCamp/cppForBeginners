d1dc06880af52a523c57adfaf47e6f69b7dda0b6
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-31 15:01:14 -0500

    Switch Statemnt

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -2,33 +2,40 @@
 
 using namespace std;
 
+string getDayOfWeek(int dayNum) {
+	string dayName;
+
+	switch (dayNum) {
+	case 0:
+		dayName = "Sunday";
+		break;
+	case 1:
+		dayName = "Monday";
+		break;
+	case 2:
+		dayName = "Tuesday";
+		break;
+	case 3:
+		dayName = "Wednesday";
+		break;
+	case 4:
+		dayName = "Thursday";
+		break;
+	case 5:
+		dayName = "Friday";
+		break;
+	case 6:
+		dayName = "Satuday";
+		break;
+	default:
+		dayName = "Invalid Day Number";
+	}
+	return dayName;
+}
+
 int main() {
 	
-	int num1, num2;
-	char op;
-
-	cout << "Enter first number: ";
-	cin >> num1;
-	cout << "Enter operator: ";
-	cin >> op;
-	cout << "Enter second number: ";
-	cin >> num2;
-
-	int result = 0;
-
-	if (op == '+') {
-		result = num1 + num2;
-	} else if (op == '-') {
-		result = num1 - num2;
-	} else if (op == '/') {
-		result = num1 / num2;
-	} else if (op == '*') {
-		result = num1 * num2;
-	} else {
-		cout << "Invalid operator";
-	}
-
-	cout << result;
+	cout << getDayOfWeek(2);
 
 	return 0;
 }
