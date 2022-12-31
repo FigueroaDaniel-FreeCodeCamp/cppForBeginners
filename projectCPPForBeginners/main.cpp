88450ceedbee121847b9cf59dd3b3d9ed6f71917
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-30 19:48:17 -0500

    If statements (con't)

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
+int getMax(int num1, int num2, int num3) {
+	int result;
+
+	if (num1 >= num2 && num1 >= num3) {
+		result = num1;
+	} else if (num2 >= num1 && num2 >= num3) {
+		result = num2;
+	} else {
+		result = num3;
+	}
+
+	return result;
+}
+
 int main() {
 	
-	bool isMale = true;
-	bool isTall = true;
-
-	if (isMale && isTall) {
-		cout << "You are a tall male";
-	} else if(isMale && !isTall) {
-		cout << "You are a short male";
-	} else if (!isMale && isTall) {
-		cout << "You are tall but not male";
-	} else {
-		cout << "You are not male and tall";
-	}
+	cout << getMax(200, 233, 90);
 
 	return 0;
 }
