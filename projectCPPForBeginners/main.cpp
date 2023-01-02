e65e13a9b599de66c13dae88a3714cc5987fc1fe
Daniel Figueroa <da.figuepa@gmail.com>
2023-01-02 17:45:13 -0500

    2d arrays and Nested Loops

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 int main() {
 	
-	int nums[] = { 2, 0, 4, 8, 7 };
-	for (int i = 0; i < 5; i++) {
-		cout << nums[i] << endl;
+	int numberGrid[3][2] = {
+		{1, 2},
+		{3, 4},
+		{5, 6}
+	};
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			cout << numberGrid[i][j];
+		}
+		cout << endl;
 	}
 
 	return 0;
