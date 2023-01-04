056ab27e80b0753dc8139f2c2a167d0bf83a9a21
Daniel Figueroa <da.figuepa@gmail.com>
2023-01-04 18:32:23 -0500

    Constructor Functions

--- projectCPPForBeginners/main.cpp
+++ projectCPPForBeginners/main.cpp
@@ -7,16 +7,27 @@ public:
 	string title;
 	string author;
 	int pages;
+
+	Book() {
+		title = "no title";
+		author = "no author";
+		pages = 0;
+	}
+	Book(string aTitle, string aAuthor, int aPages) {
+		title = aTitle;
+		author = aAuthor;
+		pages = aPages;
+	}
 };
 
 int main() {
 	
-	Book book1;
-	book1.title = "Harry Potter";
-	book1.author = "JK Rowling";
-	book1.pages = 500;
+	Book book1("Harry Potter", "JK Rowling", 500);
+	Book book2("The Lord of the Rings", "Tolkein", 900);
+	Book book3;
 
-	cout << book1.title;
+	cout << book1.title << endl;
+	cout << book3.title << endl;
 
 	return 0;
 }
